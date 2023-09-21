348806a52c56223f8a18a42d6eea78f14c66b301
Sudhir Maurya <sudhirmaurya769@gmail.com>
2023-09-21 09:11:34 +0000

    02_datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -6,6 +6,22 @@ console.log("Sudhir");
 let name = "sudhir"
 let age = 21;
 let isLoggedIn = true;
+let state = null;
 
 let num = age;
 console.log(num);
+
+
+// number => 2 to power 53
+//  bigInt
+// string = ""
+// boolean = true/false
+// null=>standalone value
+// undefined
+// symbol=> unique
+
+// object
+
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
