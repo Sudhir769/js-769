54dac0b18732a88ba599362ec1c400c88363484e
Sudhir Maurya <sudhirmaurya769@gmail.com>
2023-09-21 10:42:48 +0000

    05_comparisions

---
+++ 01_basics/05_comparisons.js
@@ -0,0 +1,21 @@
+console.log("Comparisons");
+
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2==1);
+console.log(2!=1);
+console.log(2!=1);
+console.log("2">=1);
+
+console.log("");
+
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0);//true
+
+console.log(undefined>=0);//false
+
+//strict check (===)
+console.log("1"===1); //false
