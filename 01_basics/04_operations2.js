0b0f82bffb192fe5c6e3762192dcfd8cd557b668
Sudhir Maurya <sudhirmaurya769@gmail.com>
2023-09-21 10:40:18 +0000

    04_operations

---
+++ 01_basics/04_operations2.js
@@ -0,0 +1,38 @@
+let value = 3
+console.log(-value);
+
+// console.log(2+2);
+// console.log(4-2);
+// console.log(4%2);
+// console.log(4*2);
+// console.log(4**2);
+// console.log(4/2);
+
+let str1 ="Hello";
+let str2 = " Sudhir";
+
+console.log(str1+str2);
+
+console.log(2 + "1"); //21
+console.log("1" + 2); //12
+console.log("1" + 2 + 2); //122
+console.log(2 + 2 + "1"); //41
+console.log(1 + "3"); //13
+console.log("3" + 1); //31
+console.log(1 - "3"); //-2
+console.log("3" - 1); //2
+console.log(3+4*5%3); //console.log(3+((4*5)%3)) 
+console.log(+true)//1
+console.log(+"")//0
+console.log(+"7")//7
+
+let num1, num2, num3;
+num1=num2=num3=3;
+
+let gameCounter=100;
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);//102
+
+console.log("");
+
