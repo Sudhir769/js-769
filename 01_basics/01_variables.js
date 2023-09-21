49d2f13deae15f84fb2a653a47e7d5b9a63ec6f5
Sudhir Maurya <sudhirmaurya769@gmail.com>
2023-09-21 07:15:45 +0000

    Added new Files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14453
+let accountEmail =  "sudhir769@gmail.com"
+var accountPassword = "12345"
+var accountCity = "jaipur"
+
+
+
+accountEmail = "sm@sm.com"
+accountPassword = "213141"
+accountCity = "lucknow"
+
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword,accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,6 @@
+"use strict"; // treat all js code in newer version
+
+alert(3 + 5)
+
+console.log( 7 + 5);
+console.log("Sudhir");
