868421a2dc2b7cecccde05865bd654da6807b366
Sudhir Maurya <sudhirmaurya769@gmail.com>
2023-09-26 12:18:44 +0000

    while-loop

---
+++ 05_loops/02_iterations.js
@@ -0,0 +1,19 @@
+// while-loop
+
+// JavaScript code to use while loop
+
+let val = 1;
+while (val < 6) {
+	console.log(val);
+	val += 1;
+}
+
+// do-while-loop
+// JavaScript code to use do-while loop
+
+let val2 = 1;
+do {
+	console.log(val);
+	val2 += 1;
+} while (val < 6);
+
